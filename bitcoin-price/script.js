3db87a5c46f0dcb5eace6a07668cdb40a91d7826
Juliana Chagas <julianavrchagas@gmail.com>
2022-07-20 16:31:44 -0300

    feat: add main app files

---
+++ bitcoin-price/script.js
@@ -0,0 +1,32 @@
+const currencySymbols = {
+  BRL: 'R$',
+  USD: '$',
+  EUR: '€',
+  GBP: '£'
+};
+
+function getBitcoinValue(currency) {
+  fetch('https://blockchain.info/ticker')
+    .then(response => response.json())
+    .then(data => {
+      updateUI(data[currency].buy, data[currency].symbol);
+    })
+    .catch(err => console.log(err));
+}
+
+function updateUI(value, symbol) {
+  const result = document.querySelector('.result');
+  result.textContent = `${currencySymbols[symbol]} ${value}`;
+}
+
+const selectForm = document.forms['select-currency-form'];
+selectForm.addEventListener('submit', e => {
+  e.preventDefault();
+});
+
+selectForm.addEventListener('change', () => {
+  const currencySelected = document.querySelector('#select-currency').value;
+  if (currencySelected !== 'default') {
+    getBitcoinValue(currencySelected);
+  }
+});
